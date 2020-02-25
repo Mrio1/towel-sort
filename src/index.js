d4c6d010ab96526346a3b68f19520157c179f229
Aliaksandr Zhur <sanchesmrio1@gmail.com>
2020-02-26 02:29:13 +0300

    solve 'tovel-sort' task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || !matrix.length){
+        return [];
+    }
+    let arr = [];
+    for(let i = 0; i < matrix.length; i++){
+        if(i%2==0){
+            matrix[i].forEach(element => {
+                arr.push(element);
+            });
+        } else {
+            matrix[i].reverse().forEach(element => {
+                arr.push(element)
+            })
+        }
+    }
+    return arr;
 }
